4fa03175999ef3e82956a6fe06e7d7ac96f146f3
Carlos Ferreira <carlos@especializati.com.br>
2023-04-21 09:07:39 -0300

    feat: show movies from database

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hii")
+    movies = Movie.objects.all()
+    movies = ', '.join([movie.title for movie in movies])
+    return HttpResponse(movies)
