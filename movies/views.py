aa8b0eb398925c9765f0cb3405f44bbabf37b5fb
Carlos Ferreira <carlos@especializati.com.br>
2023-04-24 09:59:16 -0300

    feat: view 404

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
 from .models import Movie
 
 # Create your views here.
@@ -9,7 +9,14 @@ def index(request):
     # return HttpResponse(movies)
     return render(request, 'movies/index.html', { 'movies': movies })
 
+# def detail(request, movie_id):
+#     try:
+#         # movie = Movie.objects.get(pk=movie_id)
+#         movie = Movie.objects.get(id=movie_id)
+#         return render(request, 'movies/detail.html', { 'movie': movie })
+#     except Movie.DoesNotExist:
+#         raise Http404()
+
 def detail(request, movie_id):
-    # movie = Movie.objects.get(pk=movie_id)
-    movie = Movie.objects.get(id=movie_id)
+    movie = get_object_or_404(Movie, pk=movie_id)
     return render(request, 'movies/detail.html', { 'movie': movie })
